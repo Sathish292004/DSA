789436250b97144113eabb0ef4a66b61bdc337d3
Sathish292004 <sathishsince2004@gmail.com>
2026-08-12 19:29:13 +0530

    implement get, set, search, and delete methods for Array class and update error logging

--- Array.java
+++ Array.java
@@ -1,3 +1,5 @@
+import javax.naming.directory.SearchControls;
+
 public class Array {
 
     int[] arr;
@@ -13,7 +15,7 @@ public class Array {
 
     boolean insert (int index, int element){
         if(index < 0 || size >= capacity || index > size){
-            System.out.println("Invaild");
+            System.out.println("Cant Insert");
             return false;
         }
         for(int i=size; i > index; i--){
@@ -25,21 +27,71 @@ public class Array {
         return true;
     }
 
+    int get(int index){
+        if(index < 0 || index >= size){
+            System.out.println("Invalid Index");
+            return -9999;
+        }
+        return arr[index];
+    }
+
+    void set(int index, int element){
+        if(index < 0 || index >= size){
+            System.out.println("Invalid Index");
+        }
+        arr[index] = element;
+    }
+
     void display(){
         for(int i=0; i<size;i++){
             System.out.print(arr[i]+ " ");
         }
+        System.out.println();
 
+    }
+
+    int search(int element){
+        for(int i=0; i<size; i++){
+            if(arr[i] == element){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    boolean delete(int index){
+        if(index < 0 || index >= size){
+            System.out.println("Cannot Delete the value");
+            return false;
+        }
+        for(int i=index; i<size-1; i++){
+            arr[i] = arr[i+1];
+        }
+        size--;
+        return false;
     }
 
     public static void main (String[] args){
 
-        Array arr1 = new Array(4);
+        Array arr1 = new Array(5);
 
         arr1.insert(0, 5);
-        arr1.insert(1, 4);
+        arr1.insert(1, 15);
+        arr1.insert(2, 10);
+        arr1.insert(3, 20);
+        arr1.insert(4, 30);
 
         arr1.display();
+
+        System.out.println("Get Value = " + arr1.get(0));
+
+        arr1.set(1,98);
+
+        arr1.delete(1);
+
+        arr1.display();
+
+        System.out.println("Search Value = " + arr1.search(20));
 
     }
 
